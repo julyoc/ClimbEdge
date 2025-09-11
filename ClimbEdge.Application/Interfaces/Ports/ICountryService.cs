f59d25eda60df2ba19529d74ec770df3e572d203
julio <julito.1998@hotmail.es>
2025-09-11 17:22:12 -0500

    add country service

--- ClimbEdge.Application/Interfaces/Ports/ICountryService.cs
+++ ClimbEdge.Application/Interfaces/Ports/ICountryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClimbEdge.Domain.ExternalEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace ClimbEdge.Application.Interfaces.Ports
 {
     public interface ICountryService
     {
+        Task<Country> GetCountryInfoAsync(string countryCode);
+        Task<IEnumerable<Country>> GetCountryInfoAsync(IEnumerable<string> countryCodes);
+        Task<IEnumerable<Country>> GetCountryInfoAsync();
     }
 }
